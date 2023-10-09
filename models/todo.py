f328e008968767a242056c66ae3e39de96ef6882
Marek Procházka <m.prochazka2002@gmail.com>
2023-10-09 14:49:00 +0200

    basic shell script utils

--- models/todo.py
+++ models/todo.py
@@ -6,5 +6,6 @@ from models.base import BaseUUIDModel
 class Todo(BaseUUIDModel, table=True):
     content: str = Field(default="!")
     done: bool = Field(default=False)
+    description: str | None = Field(default=None)
 
     __tablename__ = "todo"
